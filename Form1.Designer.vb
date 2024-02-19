8dd308ca48b7012266cfc35a8ba68e93b43c2f63
adari <adari@ADARIONOTG>
2024-02-19 12:52:07 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRentals
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grpStructures = New System.Windows.Forms.GroupBox()
+        Me.radCabin = New System.Windows.Forms.RadioButton()
+        Me.radYurt = New System.Windows.Forms.RadioButton()
+        Me.radPrimitive = New System.Windows.Forms.RadioButton()
+        Me.lblNumOfDays = New System.Windows.Forms.Label()
+        Me.txtNumberOfDays = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.grpStructures.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'grpStructures
+        '
+        Me.grpStructures.Controls.Add(Me.radCabin)
+        Me.grpStructures.Controls.Add(Me.radYurt)
+        Me.grpStructures.Controls.Add(Me.radPrimitive)
+        Me.grpStructures.Location = New System.Drawing.Point(25, 45)
+        Me.grpStructures.Name = "grpStructures"
+        Me.grpStructures.Size = New System.Drawing.Size(200, 230)
+        Me.grpStructures.TabIndex = 0
+        Me.grpStructures.TabStop = False
+        Me.grpStructures.Text = "Structures"
+        '
+        'radCabin
+        '
+        Me.radCabin.AutoSize = True
+        Me.radCabin.Location = New System.Drawing.Point(7, 103)
+        Me.radCabin.Name = "radCabin"
+        Me.radCabin.Size = New System.Drawing.Size(99, 29)
+        Me.radCabin.TabIndex = 2
+        Me.radCabin.TabStop = True
+        Me.radCabin.Text = "Cabin"
+        Me.radCabin.UseVisualStyleBackColor = True
+        '
+        'radYurt
+        '
+        Me.radYurt.AutoSize = True
+        Me.radYurt.Location = New System.Drawing.Point(7, 67)
+        Me.radYurt.Name = "radYurt"
+        Me.radYurt.Size = New System.Drawing.Size(83, 29)
+        Me.radYurt.TabIndex = 1
+        Me.radYurt.TabStop = True
+        Me.radYurt.Text = "Yurt"
+        Me.radYurt.UseVisualStyleBackColor = True
+        '
+        'radPrimitive
+        '
+        Me.radPrimitive.AutoSize = True
+        Me.radPrimitive.Location = New System.Drawing.Point(7, 31)
+        Me.radPrimitive.Name = "radPrimitive"
+        Me.radPrimitive.Size = New System.Drawing.Size(125, 29)
+        Me.radPrimitive.TabIndex = 0
+        Me.radPrimitive.TabStop = True
+        Me.radPrimitive.Text = "Primitive"
+        Me.radPrimitive.UseVisualStyleBackColor = True
+        '
+        'lblNumOfDays
+        '
+        Me.lblNumOfDays.AutoSize = True
+        Me.lblNumOfDays.Location = New System.Drawing.Point(369, 45)
+        Me.lblNumOfDays.Name = "lblNumOfDays"
+        Me.lblNumOfDays.Size = New System.Drawing.Size(172, 25)
+        Me.lblNumOfDays.TabIndex = 1
+        Me.lblNumOfDays.Text = "Number of Days:"
+        '
+        'txtNumberOfDays
+        '
+        Me.txtNumberOfDays.Location = New System.Drawing.Point(374, 73)
+        Me.txtNumberOfDays.Name = "txtNumberOfDays"
+        Me.txtNumberOfDays.Size = New System.Drawing.Size(137, 31)
+        Me.txtNumberOfDays.TabIndex = 2
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(364, 148)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(158, 65)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "Calculate Rental Fee"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(79, 334)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(111, 25)
+        Me.lblTotal.TabIndex = 4
+        Me.lblTotal.Text = "Total Due:"
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 25
+        Me.lstDisplay.Location = New System.Drawing.Point(196, 334)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(289, 79)
+        Me.lstDisplay.TabIndex = 5
+        '
+        'frmRentals
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtNumberOfDays)
+        Me.Controls.Add(Me.lblNumOfDays)
+        Me.Controls.Add(Me.grpStructures)
+        Me.Name = "frmRentals"
+        Me.Text = "Rentals"
+        Me.grpStructures.ResumeLayout(False)
+        Me.grpStructures.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents grpStructures As GroupBox
+    Friend WithEvents radCabin As RadioButton
+    Friend WithEvents radYurt As RadioButton
+    Friend WithEvents radPrimitive As RadioButton
+    Friend WithEvents lblNumOfDays As Label
+    Friend WithEvents txtNumberOfDays As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lstDisplay As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.ComponentModel.Design
+Imports System.Net.Security
+Imports System.Security.Cryptography
+
+Public Class frmRentals
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        lstDisplay.Items.Clear()
+
+        Dim cost, x, extraDays, days As Double
+        Dim primative, yurt, cabin As Double
+        primative = 20
+        yurt = 30
+        cabin = 40
+
+        If radCabin.Checked = False And radPrimitive.Checked = False And radYurt.Checked = False Then
+            MessageBox.Show("Please select a structure.")
+        End If
+
+        If IsNothing(txtNumberOfDays.Text) = True Then
+            MessageBox.Show("Please input a number.")
+            Return
+        End If
+
+        If IsNumeric(txtNumberOfDays.Text) = False Then
+            MessageBox.Show("Only Enter Numbers in the text box.")
+            Return
+        End If
+
+
+        If radPrimitive.Checked = True Then
+
+
+            If CDbl(txtNumberOfDays.Text) > 6 Then
+                extraDays = CDbl(txtNumberOfDays.Text) - 6
+                days = 6
+                x = primative - 5
+                cost = (primative * days) + (x * extraDays)
+
+            ElseIf CDbl(txtNumberOfDays.Text) <= 6 Then
+                days = CDbl(txtNumberOfDays.Text)
+                cost = days * primative
+            End If
+
+
+        ElseIf radYurt.Checked = True Then
+
+            If CDbl(txtNumberOfDays.Text) > 6 Then
+                extraDays = CDbl(txtNumberOfDays.Text) - 6
+                days = 6
+                x = yurt - 5
+                cost = (yurt * days) + (x * extraDays)
+
+            ElseIf CDbl(txtNumberOfDays.Text) <= 6 Then
+                days = CDbl(txtNumberOfDays.Text)
+                cost = days * yurt
+            End If
+
+        ElseIf radCabin.Checked = True Then
+
+            If CDbl(txtNumberOfDays.Text) > 6 Then
+                    extraDays = CDbl(txtNumberOfDays.Text) - 6
+                    days = 6
+                    x = cabin - 5
+                    cost = (cabin * days) + (x * extraDays)
+
+                ElseIf CDbl(txtNumberOfDays.Text) <= 6 Then
+                    days = CDbl(txtNumberOfDays.Text)
+                    cost = days * cabin
+                End If
+            End If
+
+
+        lstDisplay.Items.Add(cost.ToString("c2"))
+
+
+
+    End Sub
+End Class
